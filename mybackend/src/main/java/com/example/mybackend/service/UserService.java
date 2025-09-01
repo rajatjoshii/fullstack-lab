ef655bd87f8c4442b26ff4f1a4dacfd500ea414d
Rajat Joshi <rajatjoshi@Rajats-MacBook-Air.local>
2025-09-01 15:16:36 +0530

    Added password encoder and Email check

--- mybackend/src/main/java/com/example/mybackend/service/UserService.java
+++ mybackend/src/main/java/com/example/mybackend/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.mybackend.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.mybackend.repository.UserRepository;
@@ -13,36 +14,33 @@ import com.example.mybackend.exception.UserNotFoundException;
 @Service
 public class UserService {
     public final UserRepository userRepository;
+    public final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
     public Page<UserResponseDTO> getAllUsers(Pageable pageable){
         Page<User> userPage= userRepository.findAll(pageable);
-        return userPage.map(user -> new UserResponseDTO(user.getId(), user.getName()));
-        // List<User> users = userRepository.findAll();
-        // List<UserResponseDTO> newusers = new ArrayList<>();
-        // for(User user: users){
-        //     newusers.add(new UserResponseDTO(user.getId(),user.getName()));
-        // }
-        // return newusers;
+        return userPage.map(user -> new UserResponseDTO(user.getId(), user.getName(), user.getEmail()));
     }
 
     public UserResponseDTO getUserByID(int id){
         User user = userRepository.findById(id)
         .orElseThrow(() -> new UserNotFoundException("User not found with id" + id));
         
-        return new UserResponseDTO(user.getId(), user.getName());
+        return new UserResponseDTO(user.getId(), user.getName(), user.getEmail());
     }
 
     public UserResponseDTO addUser(CreateUserRequestDTO newUser){
         User user = new User();
         user.setName(newUser.getName());
         user.setPassword(newUser.getPassword());
+        user.setEmail(newUser.getEmail());
         User saved = userRepository.save(user);
-        return new UserResponseDTO(saved.getId(), saved.getName());
+        return new UserResponseDTO(saved.getId(), saved.getName(), saved.getEmail());
     }
 
     public UserResponseDTO updateUser(int id, CreateUserRequestDTO userUpdate){
@@ -50,8 +48,9 @@ public class UserService {
         .orElseThrow(() -> new UserNotFoundException("User not found with id" + id));
 
         existingUser.setName(userUpdate.getName());
+        existingUser.setEmail(userUpdate.getEmail());
         User saved = userRepository.save(existingUser);
-        return new UserResponseDTO(saved.getId(), saved.getName());
+        return new UserResponseDTO(saved.getId(), saved.getName(), saved.getEmail());
     }
 
     public void deleteUser(int id){
@@ -59,5 +58,30 @@ public class UserService {
             throw new UserNotFoundException("User not found with id" + id);
         }
         userRepository.deleteById(id);
+    }
+
+    public User createSampleUser(){
+        User newSampleUser = new User();
+        newSampleUser.setName("Rajat");
+        newSampleUser.setEmail("rajat@gmail.com");
+        String rawPassword = "querty123";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        newSampleUser.setPassword(encodedPassword);
+        return userRepository.save(newSampleUser);
+    }
+
+    public UserResponseDTO registerUser(CreateUserRequestDTO request){
+        if(userRepository.findByEmail(request.getEmail()).isPresent()){
+            throw new IllegalArgumentException("Email is already registered");
+        }
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        String rawPassord = request.getPassword();
+        String encodedPassword = passwordEncoder.encode(rawPassord);
+        user.setPassword(encodedPassword);
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDTO(savedUser.getId(), savedUser.getName(), savedUser.getEmail());
     }
 }
